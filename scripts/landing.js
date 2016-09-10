d50e7b9717c87c8359a7be0b405ef6cbbf06b5eb
Shane Boyar <shane.boyar@gmail.com>
2016-09-10 19:02:29 -0400

    Animate Icons

---
+++ scripts/landing.js
@@ -0,0 +1,33 @@
+var pointsArray = document.getElementsByClassName('info-item');
+
+var animatePoints = function(points) {
+
+    var revealPoint = function(i) {
+        points[i].style.opacity = 1;
+        points[i].style.transform = "scaleX(1) translateY(0)";
+        points[i].style.msTransform = "scaleX(1) translateY(0)";
+        points[i].style.WebkitTransform = "scaleX(1) translateY(0)";
+    }
+
+    for (var i = 0; i < points.length; i++) {
+        revealPoint(i);
+    }
+
+};
+
+window.onload = function() {
+
+    // Automatically animate the points on a tall screen where scrolling can't trigger the animation
+     if (window.innerHeight > 950) {
+        animatePoints(pointsArray);
+    }
+
+    var sellingPoints = document.getElementsByClassName('info')[0];
+    var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+
+    window.addEventListener('scroll', function(event) {
+        if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+            animatePoints(pointsArray);   
+        }
+    });
+}  
