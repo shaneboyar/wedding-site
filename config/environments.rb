efe9eb3fba4a487065c5ce9ff917d825da635264
Shane Boyar <shane.boyar@gmail.com>
2017-05-29 22:11:52 -0400

    Config prod fog carrierwave

--- config/environments.rb
+++ config/environments.rb
@@ -1,5 +1,6 @@
 configure :development do
   set :database, 'sqlite3:db/database.db'
+
   # Configure Carrierwave
   CarrierWave.configure do |config|
     config.fog_provider = 'fog/aws'
@@ -24,4 +25,15 @@ configure :production do
     database: db.path[1..-1],
     encoding: 'utf8'
   )
+
+  CarrierWave.configure do |config|
+    config.fog_provider = 'fog/aws'
+    config.fog_credentials = {
+      provider:              'AWS',
+      aws_access_key_id:     ENV['S3_ACCESS_KEY'],
+      aws_secret_access_key: ENV['S3_SECRET_KEY'],
+      region:                ENV['S3_REGION']
+    }
+    config.fog_directory = ENV['S3_BUCKET']
+  end
 end
